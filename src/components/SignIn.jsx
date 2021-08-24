1657eddbe4d6471ba0f749eb34056d0f798909d8
utsabk <kharelutsab@gmail.com>
2021-08-24 12:24:40 +0300

    SignIn form with Formik

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,8 +1,50 @@
 import React from 'react';
-import {Text} from 'react-native';
+import { Text, TextInput, Pressable, View } from 'react-native';
+import { Formik, useField } from 'formik';
+
+const initialValues = {
+    username: '',
+    password: '',
+  };
+
+const SignInForm = ({ onSubmit }) => {
+  const [usernameField, usernameMeta, usernameHelpers] = useField('username');
+  const [passwordField, passwordMeta, passwordHelpers] = useField('password');
+
+  return (
+    <View>
+      <TextInput
+        placeholder="Enter username"
+        value={usernameField.value}
+        onChangeText={(text) => usernameHelpers.setValue(text)}
+      />
+      <TextInput
+        placeholder="Enter password"
+        value={passwordField.value}
+        secureTextEntry
+        onChangeText={(text) => passwordHelpers.setValue(text)}
+      />
+
+    <Pressable onPress={onSubmit}>
+        <Text>Submit</Text>
+      </Pressable>
+    </View>
+  );
+};
 
 const SignIn = () => {
-    return <Text> The sign in view</Text>;
+  const onSubmit = (values) => {
+    const username = values.username;
+    const password = values.password;
+
+    console.log('Username:-', username, 'Password:-', password);
+  };
+
+  return (
+    <Formik initialValues={initialValues} onSubmit={onSubmit}>
+      {({handleSubmit}) => <SignInForm onSubmit={handleSubmit} />}
+    </Formik>
+  );
 };
 
 export default SignIn;
